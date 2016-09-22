4956373c457b5df80a5d4553b04143af09502cdd
Kanybek <kano.momuke@gmail.com>
2016-09-22 14:18:29 +0600

    needed files added

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "github.com/KanybekMomukeyev/testingGRPC/protolocation"
+
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type kanoserver struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *kanoserver) RpcMethod(ctx context.Context, in *pb.RpcRequest) (*pb.RpcResponse, error) {
+	return &pb.RpcResponse{ResponseParam: "Hello KOKE" + in.RequestParam}, nil
+}
+
 
 func main() {
-	fmt.Print("Hello world\n")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterRpcGreeterServer(s, &kanoserver{})
+	s.Serve(lis)
 }
+
+
