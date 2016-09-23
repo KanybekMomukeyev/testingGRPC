893cf17a67ec41a47b041e71cbbfc1fbd3928aee
Kanybek <kano.momuke@gmail.com>
2016-09-23 16:35:47 +0600

    sdfdsfds

--- client/client.go
+++ client/client.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"log"
+	"os"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "github.com/KanybekMomukeyev/testingGRPC/protolocation"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
 func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewRpcGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := client.RpcMethod(context.Background(), &pb.RpcRequest{RequestParam: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("Greeting: %s", r.ResponseParam)
 }
